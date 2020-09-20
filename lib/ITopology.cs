5e9647c5f4441d7fcd773b98402f533534b99127
Nuno Fachada <faken@fakenmc.com>
2020-09-20 20:53:13 +0100

    Add global topology

--- lib/ITopology.cs
+++ lib/ITopology.cs
@@ -5,6 +5,8 @@ namespace OpenPSO.Lib
     public interface ITopology
     {
         int PopSize { get; }
+
+        void Init(IEnumerable<Particle> particles);
         IEnumerable<Particle> GetNeighbors(int pid);
 
     }
---
+++ lib/Topologies/GlobalTopology.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace OpenPSO.Lib.Topologies
+{
+    public class GlobalTopology : ITopology
+    {
+        private IEnumerable<Particle> particles;
+        public int PopSize { get; }
+
+        public void Init(IEnumerable<Particle> particles)
+        {
+            this.particles = particles;
+        }
+
+        public IEnumerable<Particle> GetNeighbors(int pid)
+        {
+            if (particles is null)
+                throw new InvalidOperationException("Topology not initialized");
+
+            foreach (Particle p in particles)
+            {
+                if (p.id != pid) yield return p;
+            }
+        }
+    }
+}
