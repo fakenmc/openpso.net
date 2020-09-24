d64961e53205bdfb1afe3df8e1e4bd26d68ed585
Nuno Fachada <nfachada@laseeb.org>
2020-09-24 18:56:02 +0100

    Cli making several runs for testing

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -9,38 +9,43 @@ namespace OpenPSO.Cli
     {
         static void Main(string[] args)
         {
-            PSO pso = new PSO(
-                p => true,     // Update strategy
-                p => 0.729844, // inertia weight
-                p => 1.494,    // c1
-                p => 1.494,    // c2
-                p => -10,      // xMin
-                p => 10,       // xMax
-                p => 10,       // vMax
-                GroupBestPosition.Local, // Local or global best?
-                2.56,          // Initial xMin
-                5.12,          // Initial xMax
-                new Rastrigin(),  // Function
-                10,            // Number of dimensions in function
-                980_000,       // Max. evaluations
-                10,            // Criterion
-                false,         // Keep going after criteria's been met?
-                new VonNeumannGridTopology(7, 7) //new GlobalTopology(50)  // Topology
-            );
-            // new VonNeumannGridTopology(7, 7);
-            // new MooreGridTopology(7, 7);
-
-            pso.PostIteration += PrintBestSoFar;
-            pso.Run();
+            for (int i = 0; i < 30; i++)
+            {
+                PSO pso = new PSO(
+                    p => true,     // Update strategy
+                    p => 0.729844, // inertia weight
+                    p => 1.494,    // c1
+                    p => 1.494,    // c2
+                    p => -600,      // xMin
+                    p => 600,       // xMax
+                    p => 100,       // vMax
+                    300,          // Initial xMin
+                    600,          // Initial xMax
+                    new Griewank(),  // Function
+                    10,            // Number of dimensions in function
+                    980_000,       // Max. evaluations
+                    0.05,            // Criterion
+                    false,         // Keep going after criteria's been met?
+                    new GlobalTopology(49)  // Topology
+                );
+                // new VonNeumannGridTopology(7, 7);
+                // new MooreGridTopology(7, 7);
+                //pso.PostIteration += PrintBestSoFar;
+                pso.Run();
+                Console.WriteLine("{0}\t{1}\t{2}", pso.TotalEvals, pso.BestSoFar.fitness, pso.AvgFitCurr);
+            }
         }
 
         private static void PrintBestSoFar(PSO pso)
         {
             Console.Write(
-                $"Evals={pso.TotalEvals} |Fitness={pso.BestSoFar.fitness} @ (");
-            for (int i = 0; i < pso.BestSoFar.position.Count; i++)
-                Console.Write($"{pso.BestSoFar.position[i]:f2}, ");
-            Console.WriteLine(")");
+                $"Evals={pso.TotalEvals} | BestFit={pso.BestSoFar.fitness:f4}");
+            Console.Write($" | AvgFit={pso.AvgFitCurr:f4}");
+            // Console.Write(" @ (");
+            // for (int i = 0; i < pso.BestSoFar.position.Count; i++)
+            //     Console.Write($"{pso.BestSoFar.position[i]:f2}, ");
+            //Console.Write(")");
+            Console.WriteLine();
         }
     }
 }
